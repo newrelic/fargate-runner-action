c374bb83e67b793644eae4aa5f77aba93a668d49
Cristian Ciutea <cristi.ciutea@gmail.com>
2023-01-30 11:40:55 +0100

    added ci-runner docker image file

---
+++ ci-runner-image/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e 
+
+if [ -z "${REPO_NAME}" ]; then
+    printf "Repository name not specified, REPO_NAME env variable is empty\n" >&2
+    exit 1
+fi
+
+echo "$SSH_KEY" | base64 --decode > ~/.ssh/caos-dev-arm.cer
+chmod 600  ~/.ssh/caos-dev-arm.cer
+
+git clone https://github.com/"${REPO_NAME}".git /srv/"${REPO_NAME}"
+
+pushd "/srv/$REPO_NAME"
+
+git fetch origin
+git checkout "${REF}"
+git pull origin "${REF}"
+mkdir -p "${ANSIBLE_INVENTORY_FOLDER}"
+
+make "${@}"
