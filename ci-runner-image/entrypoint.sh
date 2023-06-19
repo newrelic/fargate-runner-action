9a3df56677ff6df8ff694febd0d15a95192d0c61
David Sánchez <davidsanchez@newrelic.com>
2023-06-19 14:40:45 +0100

    ci: use generated key directly

--- ci-runner-image/entrypoint.sh
+++ ci-runner-image/entrypoint.sh
@@ -9,13 +9,12 @@ fi
 
 echo "$SSH_KEY" | base64 --decode > ~/.ssh/caos-dev-arm.cer
 chmod 600  ~/.ssh/caos-dev-arm.cer
-cp ~/.ssh/caos-dev-arm.cer ~/.ssh/id_rsa
 
 if [ -z "${GIT_CLONE_URL}" ]; then
     GIT_CLONE_URL=https://github.com/"${REPO_NAME}".git
 fi
 echo "cloning repo at ${GIT_CLONE_URL}"
-git clone --config core.sshCommand="ssh -Tv -i ~/.ssh/id_rsa" "${GIT_CLONE_URL}" /srv/"${REPO_NAME}"
+git clone --config core.sshCommand="ssh -Tv -i ~/.ssh/caos-dev-arm.cer" "${GIT_CLONE_URL}" /srv/"${REPO_NAME}"
 
 pushd "/srv/$REPO_NAME"
 
